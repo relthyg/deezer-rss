ab4201fff9c9b92069c6dac50399a723f6be41c3
relthyg <relthyg@posteo.de>
2021-03-13 15:56:21 +0100

    Set default values for cache variables

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    CACHE_TYPE = os.environ.get('CACHE_TYPE')
-    CACHE_DEFAULT_TIMEOUT = int(os.environ.get('CACHE_DEFAULT_TIMEOUT'))
+    CACHE_TYPE = os.environ.get('CACHE_TYPE') or 'SimpleCache'
+    CACHE_DEFAULT_TIMEOUT = int(os.environ.get('CACHE_DEFAULT_TIMEOUT')) or 0
+    CACHE_DIR = os.environ.get('CACHE_DIR') or 'cache'
