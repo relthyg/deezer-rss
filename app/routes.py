6d9f8951a5c4683c0f8425c2727d7b9015574d7c
relthyg <martin@guethler.net>
2021-03-13 11:59:38 +0100

    Implement xml feed

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import deezer
-from flask import render_template
+from flask import render_template, request, Response
 
 from app import app, cache
 
@@ -11,7 +11,14 @@ def index(artist_id):
     artist = client.get_artist(artist_id)
     albums = artist.get_albums()
     for album in albums:
-        album.tracks = album.get_tracks()
+        tracks = album.get_tracks()
+        for track in tracks:
+            m, s = divmod(track.duration, 60)
+            track.duration_in_minutes = str(m) + ':' + str(s)
+        album.tracks = tracks
     artist.albums = albums
 
-    return render_template('feed.html', artist=artist)
+    response = Response(render_template('feed.xml', artist=artist, request_url=request.base_url))
+    response.headers['Content-Type'] = 'text/xml'
+
+    return response
