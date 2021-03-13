ac603b0518d1d624f994b964d572d92326afc727
relthyg <martin@guethler.net>
2021-03-13 13:58:18 +0100

    Remove secret key as we have no secrets

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     CACHE_TYPE = os.environ.get('CACHE_TYPE')
     CACHE_DEFAULT_TIMEOUT = int(os.environ.get('CACHE_DEFAULT_TIMEOUT'))
