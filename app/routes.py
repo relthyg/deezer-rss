d10172f9e2bda86d2515d3073968ff171b93dd52
relthyg <martin@guethler.net>
2021-03-13 09:23:14 +0100

    Implement basic functionality with html template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
+import deezer
 from flask import render_template
 
 from app import app
 
 
-@app.route('/<artist>')
-def index(artist):
+@app.route('/<artist_id>')
+def index(artist_id):
+    client = deezer.Client()
+    artist = client.get_artist(artist_id)
+    albums = artist.get_albums()
+    for album in albums:
+        album.tracks = album.get_tracks()
+    artist.albums = albums
+
     return render_template('feed.html', artist=artist)
