6e1ca8637551d0cdb6d507e218117d43a7cfa556
relthyg <martin@guethler.net>
2021-03-13 17:43:41 +0100

    Remove wrong in cast

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     CACHE_TYPE = os.environ.get('CACHE_TYPE') or 'NullCache'
-    CACHE_DEFAULT_TIMEOUT = int(os.environ.get('CACHE_DEFAULT_TIMEOUT')) or 0
+    CACHE_DEFAULT_TIMEOUT = os.environ.get('CACHE_DEFAULT_TIMEOUT') or 0
     CACHE_DIR = os.environ.get('CACHE_DIR') or 'cache'
