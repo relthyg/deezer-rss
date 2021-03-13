e0b72883d4ae03aab7a94c80f52d480339954eb0
relthyg <relthyg@posteo.de>
2021-03-13 16:30:24 +0100

    Rename variable

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,10 @@ from app import app, cache
 from app.deezer_factory import get_artist
 
 
-@app.route('/<artist_id>')
+@app.route('/<artist_id_or_name>')
 @cache.cached()
-def index(artist_id):
-    artist = get_artist(artist_id)
+def index(artist_id_or_name):
+    artist = get_artist(artist_id_or_name)
     response = Response(render_template('feed.xml', artist=artist, request_url=request.base_url))
     response.headers['Content-Type'] = 'text/xml'
 
