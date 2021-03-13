cc45a2cde255c62450cbd47e7c1f8ee7f5188f20
relthyg <relthyg@posteo.de>
2021-03-13 13:26:41 +0100

    Fix pubDate and add album duration

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,13 @@
-import deezer
 from flask import render_template, request, Response
 
 from app import app, cache
+from app.deezer_factory import get_artist
 
 
 @app.route('/<artist_id>')
 @cache.cached()
 def index(artist_id):
-    client = deezer.Client()
-    artist = client.get_artist(artist_id)
-    albums = artist.get_albums()
-    for album in albums:
-        tracks = album.get_tracks()
-        for track in tracks:
-            m, s = divmod(track.duration, 60)
-            track.duration_in_minutes = str(m) + ':' + str(s)
-        album.tracks = tracks
-    artist.albums = albums
-
+    artist = get_artist(artist_id)
     response = Response(render_template('feed.xml', artist=artist, request_url=request.base_url))
     response.headers['Content-Type'] = 'text/xml'
 
