9e8e63e4a82b430ccd7d812841d0561050b7fe9c
relthyg <relthyg@posteo.de>
2021-05-02 13:50:52 +0200

    Redirect to canonical artist name if required.
    e.g. redirect to /Opeth if the request uri was /opeth

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Response
+from flask import render_template, redirect, request, Response
 
 from app import app, cache
 from app.deezer_factory import get_artist
@@ -8,6 +8,8 @@ from app.deezer_factory import get_artist
 @cache.cached()
 def index(artist_id_or_name):
     artist = get_artist(artist_id_or_name)
+    if artist.name != artist_id_or_name:
+        return redirect(f'/{artist.name}', 302)
     response = Response(render_template('feed.xml', artist=artist, request_url=request.base_url))
     response.headers['Content-Type'] = 'text/xml'
 
