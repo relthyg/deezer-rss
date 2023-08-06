cbc860dc1cce587e415a8e0c1d720fd8814a26fd
relthyg <relthyg@posteo.de>
2023-08-06 17:50:58 +0200

    Redirect to `artist.id` instead of `artist.name`

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,8 @@ from app.deezer_client_facade import get_artist
 @cache.cached()
 def index(artist_id_or_name):
     artist = get_artist(artist_id_or_name)
-    if artist.name != artist_id_or_name:
-        return redirect(f'/{artist.name}', 302)
+    if str(artist.id) != artist_id_or_name:
+        return redirect(f'/{artist.id}', 302)
     response = Response(render_template('feed.xml', artist=artist, request_url=request.base_url))
     response.headers['Content-Type'] = 'text/xml'
 
